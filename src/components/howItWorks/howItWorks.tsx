751b6f1ab07364f75947e6f61c23444437353232
Thiffany Gonçalves <thiffanygabrielle48@gmail.com>
2026-06-13 10:20:08 +0100

    feat: criar seção como funciona da página principal

---
+++ src/components/howItWorks/howItWorks.tsx
@@ -0,0 +1,51 @@
+function HowItWorks() {
+    return (
+        <section>
+
+            <h2>Como funciona</h2>
+
+            <div>
+
+                <h3>1. Cadastre sua empresa</h3>
+
+                <p>
+                    Informe os dados da organização e do representante.
+                </p>
+
+            </div>
+
+            <div>
+
+                <h3>2. Escolha os sistemas</h3>
+
+                <p>
+                    Monte um Sistema Integrado de Gestão personalizado.
+                </p>
+
+            </div>
+
+            <div>
+
+                <h3>3. Baixe os modelos</h3>
+
+                <p>
+                    Utilize documentos prontos para implementação.
+                </p>
+
+            </div>
+
+            <div>
+
+                <h3>4. Implemente seu SGI</h3>
+
+                <p>
+                    Siga o passo a passo dos requisitos.
+                </p>
+
+            </div>
+
+        </section>
+    );
+}
+
+export default HowItWorks;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,12 @@
 import Banner from "../../components/banner/banner";
+import HowItWorks from "../../components/howItWorks/howItWorks";
 
 function Home() {
     return (
         <>
             <Banner />
+
+            <HowItWorks />
         </>
     );
 }
