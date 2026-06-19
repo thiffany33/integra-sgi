0ee5624fc8b54ddc3aeedd1a4af39080e2f2df71
Thiffany Gonçalves <thiffanygabrielle48@gmail.com>
2026-06-19 12:29:40 +0100

    feat: adicionar estrutura do requisito 4 e organização dos modelos

--- src/routes/requirement4/requirement4.tsx
+++ src/routes/requirement4/requirement4.tsx
@@ -1,8 +1,51 @@
+import { Link } from "react-router-dom";
+
 function Requirement4() {
     return (
-        <h1>
-            Requisito 4 - Contexto da Organização
-        </h1>
+        <>
+
+            <h1>
+                Requisito 4 - Contexto da Organização
+            </h1>
+
+            <p>
+                 o Requisito 4 serve para garantir que o sistema de gestão foi construído com os pés no chão
+                 conhecendo bem a empresa, quem a rodeia e como os processos se ligam entre si.
+            </p>
+
+            <h2>
+                Sub-requisitos
+            </h2>
+
+            <ul>
+
+                <li>
+                    <Link to="/requirement4_1">
+                        4.1 Compreender a organização e o seu contexto
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="/requirement4_2">
+                        4.2 Necessidades e expectativas das partes interessadas
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="/requirement4_3">
+                        4.3 Determinar o âmbito do sistema de gestão
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="/requirement4_4">
+                        4.4 Sistema de gestão e seus processos
+                    </Link>
+                </li>
+
+            </ul>
+
+        </>
     );
 }
 
